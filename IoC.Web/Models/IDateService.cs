915a831a3c79b94e89a95b107be2e086fb415298
Mert <79603812+ufcovo@users.noreply.github.com>
2023-10-01 22:48:51 +0300

    Asp.Net Core Mvc Proje - DateService class | IDateService interface

---
+++ IoC.Web/Models/IDateService.cs
@@ -0,0 +1,11 @@
+﻿namespace IoC.Web.Models
+{
+    public interface IDateService
+    {
+        DateTime GetDateTime { get; }
+    }
+
+    public interface ISingletonDateService : IDateService { }
+    public interface IScopedDateService : IDateService { }
+    public interface ITransientDateService : IDateService { }
+}
